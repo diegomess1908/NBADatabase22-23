a348809d19296d083b06bf521db0df07f79c34b0
Chris Leonard <cjleonard23@gmail.com>
2022-11-28 16:42:01 -0500

    Division Table Made

--- nba_database_schema.sql
+++ nba_database_schema.sql
@@ -9,7 +9,7 @@
 CREATE TABLE team(
     TeamName    VARCHAR(100)    NOT NULL,
     Stadium     VARCHAR(100)    NOT NULL,
-    DivName     VARCHAR(100)    NOT NULL,
+    DivName     CHAR(50)    NOT NULL,
     Win         INT             NOT NULL,
     Loss        INT             NOT NULL,
     CoachID     INT             NOT NULL,
@@ -24,7 +24,13 @@ CREATE TABLE location(
     Stadium     VARCHAR(100)    NOT NULL,
     City        CHAR(50)        NOT NULL,
     State       CHAR(50)        NOT NULL,
-    CONSTRAINT  locationPK      FOREIGN KEY(Stadium)
+    CONSTRAINT  locationFK      FOREIGN KEY(Stadium)
                                     REFERENCES team(Stadium)
     );
     
+CREATE TABLE division(
+    DivName     CHAR(50)    NOT NULL,
+    Conference  CHAR(50)    NOT NULL,
+    CONSTRAINT  divisionFK  FOREIGN KEY(DivName)
+                                REFERENCES team(DivName)
+);
