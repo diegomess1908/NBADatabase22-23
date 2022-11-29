cb074eb34c5727b996477a700e888e0abf1aff79
Chris Leonard <cjleonard23@gmail.com>
2022-11-29 15:37:13 -0500

    Added format for data that needs al 30 teams, working on player data

---
+++ best_player_data.sql
@@ -0,0 +1,80 @@
+/******************************************************************************/
+/* NBA Database - _______ Data
+/* CSC 4480_001
+/* Jack Ahmed, Sean Sautner, Chris Leonard, Diego Messmacher
+/******************************************************************************/
+
+-- INSERT INTO best_player VALUES(name, player_id, position, ppg, rpg, apg, college_nation, draft_pick, draft_year, award_won)
+
+-- for testing
+select * from best_player;
+
+-- Atlanta Hawks, Trae Young
+INSERT INTO best_player VALUES('Trae Young', 1, 'PG', 28.4, 3.8, 9.7, 'University of Oklahoma', 5, 2018, null);
+
+-- Boston Celtics, Jayson Tatum
+INSERT INTO best_player VALUES('Jayson Tatum', 2, 'SF', 26.9, 8.0, 4.4, 'Duke', 3, 2017, null);
+
+-- Brooklyn Nets, Ben Simmons
+INSERT INTO best_player VALUES('Ben Simmons', 3, 'PG', 0.0, 0.0, 0.0, 'LSU', 1, 2016, null);
+
+-- Charlotte Hornets, LaMelo Ball
+INSERT INTO best_player VALUES('LaMelo Ball', 4, 'PG', 20.1, 6.6, 7.6, 'High School', 3, 2020, null);
+
+-- Chicago Bulls, DeMar DeRozan
+INSERT INTO best_player VALUES('DeMar DeRozan', 5, 'SF', 27.9, 4.0, 4.6, 'USC', 9, 2009, null);
+
+-- Cleveland Cavaliers, Darius Garland
+INSERT INTO best_player VALUES('Darius Garland', 6, 'PG', 21.7, 3.3, 8.6, 'Vanderbilt', 5, 2019, null);
+
+-- Dallas Mavericks, Luka Doncic
+INSERT INTO best_player VALUES('Luka Doncic', 7, 'SG', 28.4, 9.2, 8.7, 'Slovania', 3, 2018, null);
+
+-- Denver Nuggets, Nikola Jokic
+INSERT INTO best_player VALUES('Nikola Jokic', 8, 'C', 27.1, 13.8, 7.9, 'Serbia', 41, 2014, 'MVP');
+
+-- Detriot Pistons, Saddiq Bey
+INSERT INTO best_player VALUES('Saddiq Bey', 9, 'SF', 16.1, 5.4, 2.8, 'Villanova', 19, 2020, null);
+
+-- Golden State Warriors, Stephen Curry
+INSERT INTO best_player VALUES('Stephen Curry', 10, 'PG', 25.5, 5.2, 6.3, 'Davidson', 7, 2009, 'FinalMVP');
+
+-- Houston Rockets
+
+-- Inidana Pacers
+
+-- LA Clippers
+
+-- LA Lakers
+
+-- Memphis Grizzlies
+
+-- Miami Heat
+
+-- Milwaukee Bucks
+
+-- Minnestoa Timberwolves
+
+-- New Orleans Pelicans
+
+-- New York Knicks
+
+-- Oklahoma City Thunder
+
+-- Orlando Magic
+
+-- Philadelphia 76ers
+
+-- Phoenix Suns
+
+-- Portland Trailbalzers
+
+-- Sacramento Kings
+
+-- San Antonio Spurs
+
+-- Toronto Raptors
+
+-- Utah Jazz
+
+-- Washington Wizards
