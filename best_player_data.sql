15334486ba96292f49577765bb2d24b68040f784
Chris Leonard <cjleonard23@gmail.com>
2022-11-29 16:08:33 -0500

    best player data done

--- best_player_data.sql
+++ best_player_data.sql
@@ -4,10 +4,9 @@
 /* Jack Ahmed, Sean Sautner, Chris Leonard, Diego Messmacher
 /******************************************************************************/
 
--- INSERT INTO best_player VALUES(name, player_id, position, ppg, rpg, apg, college_nation, draft_pick, draft_year, award_won)
+delete from best_player;
 
--- for testing
-select * from best_player;
+-- INSERT INTO best_player VALUES(name, player_id, position, ppg, rpg, apg, college_nation, draft_pick, draft_year, award_won)
 
 -- Atlanta Hawks, Trae Young
 INSERT INTO best_player VALUES('Trae Young', 1, 'PG', 28.4, 3.8, 9.7, 'University of Oklahoma', 5, 2018, null);
@@ -39,42 +38,65 @@ INSERT INTO best_player VALUES('Saddiq Bey', 9, 'SF', 16.1, 5.4, 2.8, 'Villanova
 -- Golden State Warriors, Stephen Curry
 INSERT INTO best_player VALUES('Stephen Curry', 10, 'PG', 25.5, 5.2, 6.3, 'Davidson', 7, 2009, 'FinalMVP');
 
--- Houston Rockets
+-- Houston Rockets, Jalen Green
+INSERT INTO best_player VALUES('Jalen Green', 11, 'SG', 17.3, 3.4, 2.6, 'GLeague Ignite', 2, 2021, null);
 
--- Inidana Pacers
+-- Inidana Pacers, Tyrese Haliburton
+INSERT INTO best_player VALUES('Tyrese Haliburton', 12, 'PG', 15.3, 4.0, 8.2, 'Iowa St', 12, 2021, null);
 
--- LA Clippers
+-- LA Clippers, Kawhi Leonard
+INSERT INTO best_player VALUES('Kawhi Leonard', 13, 'SF', 0.0, 0.0, 0.0, 'San Diego St', 15, 2011, null);
 
--- LA Lakers
+-- LA Lakers, LeBron James
+INSERT INTO best_player VALUES('LeBron James', 14, 'SF', 30.3, 8.2, 6.2, 'High School', 1, 2003, null);
 
--- Memphis Grizzlies
+-- Memphis Grizzlies, Ja Morant
+INSERT INTO best_player VALUES('Ja Morant', 15, 'PG', 27.4, 5.7, 6.7, 'Murray St', 2, 2019, 'MostImprovedPlayer');
 
--- Miami Heat
+-- Miami Heat, Jimmy Butler
+INSERT INTO best_player VALUES('Jimmy Butler', 16, 'SF', 21.4, 5.9, 5.5, 'Marquette', 30, 2011, null);
 
--- Milwaukee Bucks
+-- Milwaukee Bucks, Giannis Antetokoumnpo
+INSERT INTO best_player VALUES('Giannis Antetokoumnpo', 17, 'PF', 29.9, 11.6, 5.8, 'Greese', 15, 2013, null);
 
--- Minnestoa Timberwolves
+-- Minnestoa Timberwolves, Karl-Anthony Towns
+INSERT INTO best_player VALUES('Karl-Anthony Towns', 18, 'C', 24.6, 9.8, 3.6, 'Kentucky', 1, 2015, null);
 
--- New Orleans Pelicans
+-- New Orleans Pelicans, Zion Williamson
+INSERT INTO best_player VALUES('Zion Williamson', 19, 'PF', 0.0, 0.0, 0.0, 'Duke', 1, 2019, null);
 
--- New York Knicks
+-- New York Knicks, RJ Barrett
+INSERT INTO best_player VALUES('RJ Barrett', 20, 'SF', 20.0, 5.8, 3.0, 'Duke', 1, 2019, null);
 
--- Oklahoma City Thunder
+-- Oklahoma City Thunder, Shai Gilgeous-Alexander
+INSERT INTO best_player VALUES('Sha Gilgeous-Alexander', 21, 'PG', 24.5, 5.0, 5.9, 'Kentucky', 11, 2018, null);
 
--- Orlando Magic
+-- Orlando Magic, Mo Bamba
+INSERT INTO best_player VALUES('Mo Bamba', 22, 'C', 10.6, 8.1, 1.2, 'Texas', 5, 2018, null);
 
--- Philadelphia 76ers
+-- Philadelphia 76ers, Joel Embiid
+INSERT INTO best_player VALUES('Joel Embiid', 23, 'C', 30.6, 11.7, 4.2, 'Kansas', 3, 2014, null);
 
--- Phoenix Suns
+-- Phoenix Suns, Devin Booker
+INSERT INTO best_player VALUES('Devin Booker', 24, 'SG', 26.8, 5.0, 4.8, 'Kentucky', 13, 2015, null);
 
--- Portland Trailbalzers
+-- Portland Trailbalzers, Damian Lillard
+INSERT INTO best_player VALUES('Damian Lillard', 25, 'PG', 24.0, 4.1, 7.3, 'Weber St', 6, 2012, null);
 
--- Sacramento Kings
+-- Sacramento Kings, De'Aaron Fox
+INSERT INTO best_player VALUES('DeArron Fox', 26, 'PG', 23.2, 3.9, 5.6, 'Kentucky', 5, 2017, null);
 
--- San Antonio Spurs
+-- San Antonio Spurs, Keldon Johnson
+INSERT INTO best_player VALUES('Keldon Johnson', 27, 'PF', 17, 6.1, 2.1, 'Kentucky', 29, 2019, null);
 
--- Toronto Raptors
+-- Toronto Raptors, Scottie Barnes
+INSERT INTO best_player VALUES('Pascal Siakam', 28, 'PF', 22.8, 8.5, 5.3, 'New Mexico St', 27, 2016, null);
 
--- Utah Jazz
+-- Utah Jazz, Donovan Mitchell
+INSERT INTO best_player VALUES('Donovan Mitchell', 29, 'SG', 25.9, 5.2, 4.3, 'Louisville', 13, 2017, null);
 
--- Washington Wizards
+-- Washington Wizards, Bradley Beal
+INSERT INTO best_player VALUES('Bradley Beal', 30, 'SG', 23.2, 4.7, 6.6, 'Florida', 3, 2012, null);
+
+--testing to check
+--select * from best_player;
