82d9851577849fdc4b07cbd0e09538fe50e635cb
Chris Leonard <cjleonard23@gmail.com>
2022-12-07 23:08:51 -0500

    updated schema

---
+++ Queries/nba_trigger.sql
@@ -0,0 +1,11 @@
+/******************************************************************************/
+/* NBA Database - Triggers
+/* CSC 4480_001
+/* Jack Ahmed, Sean Sautner, Chris Leonard, Diego Messmacher
+/******************************************************************************/
+
+select * from best_player;
+select * from team;
+select * from award;
+
+create trigger 
